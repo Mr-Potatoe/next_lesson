78c854789bd11c2c5e3d7297e559a9d4fd858699
Mr Potatoe <jaymeajarns@gmail.com>
2025-01-14 18:59:09 +0800

    sad

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,7 +1,7 @@
 import Navigation from "@/components/about/navigation";
-import PostsList from "@/components/posts/PostsList";
-import SearchBar from "@/components/posts/SearchBar";
-import CreatePostButton from "@/components/posts/CreatePostButton";
+// import PostsList from "@/components/posts/PostsList";
+// import SearchBar from "@/components/posts/SearchBar";
+// import CreatePostButton from "@/components/posts/CreatePostButton";
 
 export default function Home() {
   return (
@@ -11,11 +11,11 @@ export default function Home() {
         <div className="mb-8">
           <h1 className="text-4xl font-bold text-gray-900 mb-4">Posts</h1>
           <div className="flex justify-between items-center gap-4">
-            <SearchBar />
-            <CreatePostButton />
+            {/* <SearchBar />
+            <CreatePostButton /> */}
           </div>
         </div>
-        <PostsList />
+        {/* <PostsList /> */}
       </main>
     </div>
   );
